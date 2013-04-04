96d27ac6e28f91670a10c0ffec57d0a7f19882ce
Stephanie Oh <oh.stephanie.s@gmail.com>
2013-04-03 22:58:36 -0400

    stupid

--- 12.py
+++ 12.py
@@ -1,13 +1,48 @@
-d=0
-T=1
-n=1
-while d<=500:
-	d=0
+import math
+
+def primef(n,l):
+	m=n;
+	i=2;
+	while i<=int(math.ceil(math.sqrt(n)))+1:
+		if n%i==0:
+			n/=i
+			l.append(i)
+		else:
+			i+=1
+	if n!=1:
+		l.append(n)
+	return l
+
+def divisorsf(n,l):
+	r=[];
+	m=0;
+	p=0;
+	primef(n,l)
+	print len(l)
+	for x in xrange(0,len(l)-1):
+		m+=1
+		if l[x]!=l[x+1]:
+			r.append(m+1)
+			m=0;
+	for x in xrange(0,len(r)):
+		p*=r[x]
+	return p
+
+
+
+
+
+l=[];
+n=1;
+while len(l)<=5:
+	l=[];
 	T=n*(n+1)/2
-	for x in xrange(1,T):
-		if T%x==0:
-			d+=1;
+	divisorsf(T,l)
 	n+=1
-	print T
 
 print T
+print l
+
+
+
+
